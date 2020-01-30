3ca49f0f2fcf433f6b199089832fb7d6d71bcc49
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2020-01-31 01:13:02 +0530

    serve static files

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const net = require('net')
-
-// net.createServer(socket => {
-//   socket.on('data', function (data) {
-//     console.log('Echoing: %s', data.toString())
-//     socket.write(data.toString())
-//   })
-// }).listen(8000)
+const path = require('path')
+const dir = path.join(__dirname, 'public')
+const fs = require('fs')
+const fileExt = {
+  html: 'text/html',
+  txt: 'text/plain',
+  css: 'text/css',
+  gif: 'image/gif',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  js: 'application/javascript'
+}
 
 const server = net.createServer()
 
@@ -14,8 +20,58 @@ server.on('connection', function (socket) {
   console.log('server is connected' + address)
 
   socket.on('data', function (data) {
-    console.log(data)
-    socket.write(data + ':from server')
+    let body
+    data = String(data)
+    if (!data) return
+    const [method, url, protocol] = data.split(/\n/)[0].split(' ')
+    var reqpath = url.toString().split('?')[0]
+    if (method !== 'GET') {
+      body = 'Method not implemented'
+      socket.write('HTTP/1.1 501 Not Implemented\n')
+      socket.write('Content-Type: text/plain\n')
+      socket.write('Content-Length: ' + body.length + '\n\n')
+      socket.write(body)
+      socket.destroy()
+      return
+    }
+    var file = path.join(dir, reqpath.replace(/^\/$/, '/index.html'))
+    fs.access(file, (err) => {
+      if (err) {
+        body = 'Not Found'
+        socket.write('HTTP/1.1 404 Not Found\n')
+        socket.write('Content-Type: text/plain\n')
+        socket.write('Content-Length: ' + body.length + '\n\n')
+        socket.write(body)
+        socket.destroy()
+      }
+    })
+    fs.access(file, fs.constants.R_OK, (err) => {
+      if (err) {
+        body = 'Forbidden'
+        socket.write('HTTP/1.1 403 Forbidden\n')
+        socket.write('Content-Type: text/plain\n')
+        socket.write('Content-Length: ' + body.length + '\n\n')
+        socket.write(body)
+        socket.destroy()
+      }
+    })
+    const type = fileExt[path.extname(file).slice(1)] || 'text/plain'
+    fs.readFile(file, function (err, data) {
+      if (err) {
+        body = 'Not Found'
+        socket.write('HTTP/1.1 404 Not Found\n')
+        socket.write('Content-Type: text/plain\n')
+        socket.write('Content-Length: ' + body.length + '\n\n')
+        socket.write(body)
+        socket.destroy()
+      } else {
+        socket.write('HTTP/1.1 200 OK\n')
+        socket.write('Content-Type: ' + type + '\n')
+        socket.write('Content-Length: ' + data.byteLength + '\n\n')
+        socket.write(data)
+        socket.destroy()
+      }
+    })
   })
   socket.once('close', function () {
     console.log('closing the connection on ' + address)
