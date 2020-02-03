2204148d9eb4721817314105347206ebca4e0f9d
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2020-02-04 02:04:06 +0530

    routing map and url params

--- index.js
+++ index.js
@@ -4,9 +4,38 @@ const reqHeader = require('./reqHeader')
 const serveStatic = require('./staticfiles')
 
 const server = net.createServer()
-const check = async function (req, res) {
+
+const routeMap = {
+  getRoutes: new Map(),
+  getUrlRoutes: new Map(),
+  postRoutes: new Map(),
+  postUrlRoutes: new Map()
+}
+const matchRoutes = {
+  GET: ['getRoutes', 'getUrlRoutes'],
+  POST: ['postRoutes', 'postUrlRoutes']
+}
+
+function get (route, fn) {
+  const cond = route.includes('/:')
+  if (cond) routeMap.getUrlRoutes.set(route, fn)
+  if (!cond) routeMap.getRoutes.set(route, fn)
+}
+
+function post (route, fn) {
+  const cond = route.includes('/:')
+  if (cond) routeMap.postUrlRoutes.set(route, fn)
+  if (!cond) routeMap.postRoutes.set(route, fn)
+}
+const check = function (req, res) {
   res.status(200).send('reached hereeee')
 }
+const urlCheck = function (req, res) {
+  res.status(200).send('reached...' + req.params.id)
+}
+get('/check', check)
+get('/urlcheck/:id', urlCheck)
+
 server.on('connection', function (socket) {
   const address = socket.remoteAddress + ':' + socket.remotePort
   console.log('connection created on ' + address)
@@ -23,14 +52,30 @@ server.on('connection', function (socket) {
       res.send = (data) => {
         const now = new Date()
         const expiry = new Date().setDate(now.getDate() + 7)
-        socket.write('Content-Type: text/plain\n')
-        socket.write('Date :' + now + '\n')
+        socket.write('Content-Type: text/plain\nDate :' + now + '\n')
         socket.write('Expires :' + new Date(expiry) + '\n')
         socket.write('Content-Length:' + data.length + '\n\n')
-        socket.write(data)
+        socket.write(Buffer.from(data, 'utf8'))
         socket.destroy()
       }
-      if (reqObj.reqPath === '/check') await check(reqObj, res)
+      const [route, urlRoute] = matchRoutes[reqObj.method]
+      const matchFn = routeMap[route].get(reqObj.reqPath)
+      if (matchFn) return matchFn(reqObj, res)
+      const urlPathArray = reqObj.reqPath.split('/').slice(1)
+      for (const [key, value] of routeMap[urlRoute]) {
+        reqObj.params = {}
+        let flag = 0
+        const routePathArray = key.split('/').slice(1)
+        if (urlPathArray.length === routePathArray.length) {
+          for (const index in urlPathArray) {
+            if (routePathArray[index] !== urlPathArray[index]) {
+              if (!routePathArray[index].startsWith(':')) { flag = 1; break }
+              reqObj.params[routePathArray[index].slice(1)] = urlPathArray[index]
+            }
+          }
+          if (!flag) { value(reqObj, res); break }
+        }
+      }
     }
   })
   socket.once('close', function () {
